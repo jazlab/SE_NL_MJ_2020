39b33b9c8cba3a03ee5c0179383000a31b281eca
Nhat Le <lmn93beo@yahoo.com>
2018-10-27 13:56:44 -0400

    Updated plots

--- PlotTools/subject_simulation_figure_RSG.m
+++ PlotTools/subject_simulation_figure_RSG.m
@@ -2,13 +2,19 @@ close all;
 
 load SWE_sim_results.mat
 
-figure;
-subplot('121')
+figure('Position', [100, 200, 1000, 600]);
+h1 = subplot('121');
 errorbar(tss, mtp_in(:,1), stdtp_in(:,1),...
-    'r')
+    'r', 'Marker', 'o', 'LineStyle', 'none',...
+    'MarkerFaceColor', 'r', 'MarkerSize', 4,...
+    'LineWidth', 1.2);
 hold on;
 errorbar(durations + 5, prod_time_lst * 10,...
-    prod_var_lst * 10, 'b')
+    prod_var_lst * 10, 'b', 'Marker', 'o', 'LineStyle', 'none',...
+    'MarkerFaceColor', 'b', 'MarkerSize', 4,...
+    'LineWidth', 1.2)
+plot(durations + 5, prod_time_lst * 10, 'b')
+plot(durations, mtp_in(:,1), 'r')
 xlim([500, 1100])
 ylim([500, 1100])
 xlabel('$t_s (ms)$')
@@ -18,13 +24,22 @@ mymakeaxis(gca, 'xytitle', '1-2-Go', ...
     'xticks', [600, 800, 1000], 'yticks', [600 800 1000],...
     'interpreter', 'latex')
 
+axis square
 
-subplot('122')
+h2 = subplot('122');
 errorbar(tss, mtp_in(:,2), stdtp_in(:,2),...
-    'r')
+    'r', 'Marker', 'o', 'LineStyle', 'none',...
+    'MarkerFaceColor', 'r', 'MarkerSize', 4,...
+    'LineWidth', 1.2)
 hold on;
 errorbar(durations + 5, prod_time_lst2 * 10,...
-    prod_var_lst2 * 10, 'b')
+    prod_var_lst2 * 10, 'b', 'Marker', 'o', 'LineStyle', 'none',...
+    'MarkerFaceColor', 'b', 'MarkerSize', 4,...
+    'LineWidth', 1.2)
+
+plot(durations + 5, prod_time_lst2 * 10, 'b')
+plot(durations, mtp_in(:,2), 'r')
+
 xlim([500, 1100])
 ylim([500, 1100])
 xlabel('$t_s (ms)$')
@@ -33,4 +48,9 @@ plotUnity;
 mymakeaxis(gca, 'xytitle', '1-2-3-Go',...
     'xticks', [600, 800, 1000], 'yticks', [600 800 1000],...
     'interpreter', 'latex')
+
+axis square
+
+set(h1, 'Position', [0.05, 0.11, 0.43, 0.82]);
+set(h2, 'Position', [0.5, 0.11, 0.43, 0.82]);
 
